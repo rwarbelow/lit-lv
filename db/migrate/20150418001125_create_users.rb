99d661f83f7bf7c57f7864932c47653fd4cb57b2
Konr Larson <larsonkonr@gmail.com>
2015-04-17 18:12:49 -0600

    user model

---
+++ db/migrate/20150418001125_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.string :email
+      t.string :first_name
+      t.string :phone
+      t.string :role
+
+      t.timestamps null: false
+    end
+  end
+end
